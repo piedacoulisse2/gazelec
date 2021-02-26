dae400bcf85cbb8459922a1545c9eb53d5fdb55a
piedacoulisse2 <74360897+piedacoulisse2@users.noreply.github.com>
2021-02-26 19:09:54 +0100

    maj index

--- index.py
+++ index.py
@@ -3,7 +3,25 @@ import streamlit as st
 # working with sample data.
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+
 
 st.title('Application Elec gaz')
+
+adresse ='C:\\Users\\4799XA\\OneDrive\\CODE\\gazelec\\Donnees\\'
+nom ='consommation-quotidienne-brute.csv'
+lien = adresse + nom
+
+df = pd.read_csv(lien,sep=";")
+df['date'] = pd.to_datetime(df['Date'])
+df['horodate'] = pd.to_datetime(df['Date - Heure'])
+
+plt.scatter(x=df['horodate'],y=df['Consommation brute électricité (MW) - RTE'],maker='-');
+plt.scatter(x=df['horodate'],y=df['Consommation brute gaz totale (MW PCS 0°C)']);
+
+st.write(df)
+
 
 
