c0d59b2873e33032be3e45b0d8974f072961a14a
piedacoulisse2 <74360897+piedacoulisse2@users.noreply.github.com>
2021-03-01 16:31:36 +0100

    index

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import altair as alt
+from bokeh.plotting import figure
 
 st.title('Application Elec gaz')
 
@@ -11,7 +12,6 @@ df = pd.read_csv(lien, sep=";")
 
 @st.cache
 def importer_donnee():
-
     df['date'] = pd.to_datetime(df['Date'])
     df['horodate'] = pd.to_datetime(df['Date - Heure']) # This makes the function take 2s to run
 
@@ -20,7 +20,18 @@ def importer_donnee():
 importer_donnee()
 
 #st.write(df)
-
 data = df['Consommation brute électricité (MW) - RTE']
+x = df['date']
 
-st.line_chart(y=data,x=df['date'])
+chart_data = pd.DataFrame(
+     df,
+     columns=['Consommation brute électricité (MW) - RTE'])
+
+st.line_chart(chart_data)
+p = figure(
+     title='simple line example',
+     x_axis_label='x',
+     y_axis_label='y')
+p.line(x, data, legend='Trend', line_width=2)
+st.bokeh_chart(p, use_container_width=True)
+
